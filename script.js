69c418f78c24937fbad11d309775c713c5bae229
Danielle Crosswell <d.crosswell@columbia.edu>
2016-12-07 18:44:29 -0500

    added todos to js file

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-url = 'https://maps.googleapis.com/maps/api/js?key=' + KEY + '&libraries=places';
+//url = 'https://maps.googleapis.com/maps/api/js?key=' + KEY + '&libraries=places';
+
+//TODO: autofill in search
+//TODO: Make filters (use in the search API call)
+//TODO: search places API for "things to do in" + city entered
+
+//AFTER HITTING search
+//TODO: bring up map with markers
+//TODO: if someone clicks on marker, bring up info about that place 
+//TODO: be able to click on a place and add to itinerary
+//TODO: allow user to mark a place as already visited
+
+//ONCE USER CLICKS MAKE DIRECTIONS
+//TODO: code to find shortest distance using distance matrix API
+//TODO: code to give the user their directions
